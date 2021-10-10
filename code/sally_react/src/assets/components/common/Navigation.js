f18767ffc2f4df3dd2266c8651a688398e9be4e5
KCHamp <goddhqj8000@ajou.ac.kr>
2021-10-10 18:57:38 +0900

    10/10 react test

---
+++ code/sally_react/src/assets/components/common/Navigation.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Logo from './../../images/logo.png'
+import './Navigation.css'
+
+export default function Navigation_bar() {
+    const login = () => {
+        return 
+    }
+    return (
+        <header id='navHeader'>
+          <div className='container'>
+        <h1 class="logo">
+        <img src = {Logo} alt='logo'/>
+        </h1>
+  
+        <nav>
+          <ul>
+            <li><a href="#serviceFunctions">서비스 소개</a></li>
+            <li><a href="#survey">건강 분석</a></li>
+            <li><a href="#products">추천 제품 보기</a></li>
+            <li><a href="#feedback">고객 후기</a></li>
+            <li><a href="#">고객센터</a></li>
+            <li><a href="#">로그인</a></li>
+          </ul>
+        </nav>
+        </div>
+  
+      </header>
+
+    )
+}
