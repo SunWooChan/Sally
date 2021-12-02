84b0496fa11dca7d1725e3d2c92f6caa5533c8da
KCHamp <goddhqj8000@ajou.ac.kr>
2021-12-03 04:51:23 +0900

    server.js response

--- code/sally_react/server/server.js
+++ code/sally_react/server/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 
-const app = express(); // express °´Ã¼
+const app = express(); // express ï¿½ï¿½Ã¼
 var cmd = require("node-cmd");
 const db = require("./models/index");
 const { QnA } = db;
 
-// ¹Ìµé¿þ¾î : ¼­¹ö·Î¿Â ¸ðµç req¸¦ Ã³¸®ÇÑ´Ù.
-app.use(express.json()); // json¸Þ½îµå´Â ¼­¹ö·Î¿Â reqÀÇ body¿¡ json µ¥ÀÌÅÍ°¡ Á¸ÀçÇÒ °æ¿ì reqÀÇ body ÇÁ·ÎÆÛÆ¼·Î ¼³Á¤
+// ï¿½Ìµï¿½ï¿½ï¿½ï¿½ : ï¿½ï¿½ï¿½ï¿½ï¿½Î¿ï¿½ ï¿½ï¿½ï¿½ reqï¿½ï¿½ Ã³ï¿½ï¿½ï¿½Ñ´ï¿½.
+app.use(express.json()); // jsonï¿½Þ½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Î¿ï¿½ reqï¿½ï¿½ bodyï¿½ï¿½ json ï¿½ï¿½ï¿½ï¿½ï¿½Í°ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ reqï¿½ï¿½ body ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Æ¼ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
@@ -19,26 +19,27 @@ app.use(cors());
 app.post("/survey", async (req, res) => {
   const newSurvey = req.body;
   const qna = await QnA.create(newSurvey);
+  var resData = ''
   cmd.run(
     /////////////// python shell script ///////////////
     "/Users/seon-uchan/opt/anaconda3/bin/python /Users/seon-uchan/Desktop/FullStack/Sally/code/res_vege/res_vege3.py",
     ///////////////////////////////////////////////////
     function (error, success, stderr) {
       if (error) {
-        console.log("ERROR ¹ß»ý :\n\n", error);
+        console.log("ERROR ï¿½ß»ï¿½ :\n\n", error);
       } else {
-        console.log(success);
+        resData = success
       }
     }
   );
-  res.send(qna);
+  res.send(resData);
 });
 
-// 3. Çà·Ä°ö Å×ÀÌºí surprise ÄÚµå µ¹¾Æ°¡°Ô ÇÏ±â
+// 3. ï¿½ï¿½Ä°ï¿½ ï¿½ï¿½ï¿½Ìºï¿½ surprise ï¿½Úµï¿½ ï¿½ï¿½ï¿½Æ°ï¿½ï¿½ï¿½ ï¿½Ï±ï¿½
 
-// 4. surprise ÄÚµå °á°ú ÇÁ·ÐÆ®·Î º¸³»ÁÖ±â
+// 4. surprise ï¿½Úµï¿½ ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½Æ®ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Ö±ï¿½
 
-// path ,¹öÀü, mircroservice architecture
+// path ,ï¿½ï¿½ï¿½ï¿½, mircroservice architecture
 app.listen(3001, () => {
   console.log("Server is listening...");
 });
