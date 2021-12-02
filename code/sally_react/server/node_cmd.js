e5c09c129412c9146e42f5ae4dedbf86b6a3d3b4
SunWooChan <swc5770@naver.com>
2021-12-03 04:12:47 +0900

    1) server.js json type complete 2) topping img update

--- code/sally_react/server/node_cmd.js
+++ code/sally_react/server/node_cmd.js
@@ -1,29 +1,29 @@
 const express = require("express");
 var cmd = require("node-cmd");
-const fs = require('fs');
+const fs = require("fs");
 
 cmd.run(
   /////////////// python shell script ///////////////
-  "/Users/seon-uchan/opt/anaconda3/bin/python /Users/seon-uchan/Desktop/FullStack/kchTest/sally_prac2/res_vege/res_vege3.py",
+  "/Users/seon-uchan/opt/anaconda3/bin/python /Users/seon-uchan/Desktop/FullStack/Sally/code/res_vege/res_vege3.py",
   ///////////////////////////////////////////////////
   function (error, success, stderr) {
     if (error) {
       console.log("ERROR :\n\n", error);
     } else {
-      console.log("SUCCESS :\n\n", success);
+      const cmd_result = console.log(success);
     }
   }
 );
 
-fs.readFile('./package3.json', 'utf8', (error, jsonFile) => {
-  if (error) return console.log(error);
-  console.log(jsonFile);
+// fs.readFile('./package3.json', 'utf8', (error, jsonFile) => {
+//   if (error) return console.log(error);
+//   console.log(jsonFile);
 
-  const jsonData = JSON.parse(jsonFile);
-  console.log(jsonFile);
+//   const jsonData = JSON.parse(jsonFile);
+//   console.log(jsonFile);
 
-  const todos = jsonData.todos;
-  todos.forEach(todo => {
-      console.log(todo);
-  });
-});
+//   const todos = jsonData.todos;
+//   todos.forEach(todo => {
+//       console.log(todo);
+//   });
+// });
